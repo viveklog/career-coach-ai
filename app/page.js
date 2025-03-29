a317462a3305992d816cc598755ef38868427a56
viveklog <contactvivekray@gmail.com>
2025-03-29 11:06:11 +0530

    git setup doen for the main branch

--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div>
    <h1>Hello this is Vivek....</h1>
+   <p>Hi this is the paragraph to chekc the coeoe is workign afnd fkd</p>
+   </div>
   );
 }
